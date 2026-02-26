10dd5bf70bc5b7dfed1b5ffa854321abc6e13677
jupitters <sampaiobale@gmail.com>
2026-02-26 12:18:33 -0300

    about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import LandingPage from './components/LandingPage'
+import AboutPage from './components/AboutPage'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
       <>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/about" element={<AboutPage />} />
       </Routes>
       </>
   )
