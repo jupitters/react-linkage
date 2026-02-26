ccb325c3e97a71fb9484b4330f32986f2d83aacf
jupitters <sampaiobale@gmail.com>
2026-02-26 13:10:18 -0300

    footer component

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import LandingPage from './components/LandingPage'
 import AboutPage from './components/AboutPage'
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
       <>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/about" element={<AboutPage />} />
-      </Routes>
+      <NavBar />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+      <Footer />
       </>
   )
 }
