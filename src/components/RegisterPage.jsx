a66ed0c9df02d82b4e412b6c180abc314ff9f6d2
jupitters <sampaiobale@gmail.com>
2026-02-27 18:38:34 -0300

    register page

--- src/components/RegisterPage.jsx
+++ src/components/RegisterPage.jsx
@@ -2,12 +2,25 @@ import React from 'react'
 import { useForm } from 'react-hook-form'
 
 const RegisterPage = () => {
-    const {register, handleSubmit, reset, formState: {errors}} = useForm();
+    const {register, handleSubmit, reset, formState: {errors}} = useForm(
+        {
+            defaultValues: {
+                username: "",
+                email: "",
+                password: ""
+            },
+            mode: "onTouched",
+        }
+    );
+
+    const registerHandler = async (data) => {
+        
+    }
 
   return (
     <div className='min-h-[calc(100vh-64px)] flex justify-center items-center'>
-        <form>
-
+        <form onSubmit={handleSubmit(registerHandler)} className='sm:w-[450px] w-[360px] shadow-custom py-8 sm:px-8 px-4 rounded-md'>
+        <h1 className='text-center font-serif text-btnColor font-bold lg:text-3xl text-2xl'>Register Here</h1>
         </form>
     </div>
   )
