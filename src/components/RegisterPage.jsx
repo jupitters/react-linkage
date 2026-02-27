5726a0718fdbcd70f65883fec71061f8ffa9a101
jupitters <sampaiobale@gmail.com>
2026-02-27 18:42:30 -0300

    register page

--- src/components/RegisterPage.jsx
+++ src/components/RegisterPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
+import TextField from './TextField'
 
 const RegisterPage = () => {
     const {register, handleSubmit, reset, formState: {errors}} = useForm(
@@ -21,6 +22,19 @@ const RegisterPage = () => {
     <div className='min-h-[calc(100vh-64px)] flex justify-center items-center'>
         <form onSubmit={handleSubmit(registerHandler)} className='sm:w-[450px] w-[360px] shadow-custom py-8 sm:px-8 px-4 rounded-md'>
         <h1 className='text-center font-serif text-btnColor font-bold lg:text-3xl text-2xl'>Register Here</h1>
+        <hr className='mt-2 mb-5 text-black' />
+        <div className='flex flex-col gap-3'>
+            <TextField 
+                label="Username"
+                required
+                id="username"
+                type="text"
+                message="*Username is required!"
+                placeholder="Type your username"
+                register={register}
+                errors={errors}
+            />
+        </div>
         </form>
     </div>
   )
