b90fcbdf41551a1af7f1a74bc4c973d40f5f394b
jupitters <sampaiobale@gmail.com>
2026-02-28 13:45:58 -0300

    login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import TextField from './TextField'
+import { Link, useNavigate } from 'react-router-dom'
+import api from '../api/api'
+import toast from 'react-hot-toast'
 
 const Login = () => {
+  const navigate = useNavigate()
+    const [loader, setLoader] = useState(false)
+
+    const {register, handleSubmit, reset, formState: {errors}} = useForm(
+        {
+            defaultValues: {
+                username: "",
+                email: "",
+                password: ""
+            },
+            mode: "onTouched",
+        }
+    );
+
+    const loginHandler = async (data) => {
+        setLoader(true)
+        try{
+            
+            toast.success("Login Successfull!")
+        } catch (err){
+            console.log(err)
+            toast.error("Login Failed!")
+        } finally {
+            setLoader(false)
+        }
+    }
+
   return (
-    <div>Login</div>
+    <div className='min-h-[calc(100vh-64px)] flex justify-center items-center'>
+        <form onSubmit={handleSubmit(loginHandler)} className='sm:w-[450px] w-[360px] shadow-custom py-8 sm:px-8 px-4 rounded-md'>
+        <h1 className='text-center font-serif text-btnColor font-bold lg:text-3xl text-2xl'>Login Here</h1>
+        <hr className='mt-2 mb-5 text-black' />
+        <div className='flex flex-col gap-3'>
+            <TextField 
+                label="Username"
+                required
+                id="username"
+                type="text"
+                message="Username is required!"
+                placeholder="Type your username"
+                register={register}
+                errors={errors}
+            />
+            <TextField 
+                label="Password"
+                required
+                id="password"
+                type="password"
+                message="password is required!"
+                placeholder="Type your password"
+                register={register}
+                min={6}
+                errors={errors}
+            />
+        </div>
+        <button disabled={loader} type='submit' className='bg-customRed font-semibold text-white  bg-custom-gradient w-full py-2 hover:text-slate-400 transition-colors duration-100 rounded-sm my-3'>
+            {loader ? "Loading..." : "Login"}
+        </button>
+
+        <p className='text-center text-sm text-slate-700 mt-6'>Don't have and account? <Link className='font-semibold underline hover:text-black' to="/register"><span className='text-btnColor'>Register here!</span></Link></p>
+        </form>
+    </div>
   )
 }
 
