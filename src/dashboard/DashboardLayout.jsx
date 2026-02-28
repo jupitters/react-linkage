2858019f82db426d03e65f53be8da589a62a0666
jupitters <sampaiobale@gmail.com>
2026-02-28 14:46:29 -0300

    graph

--- src/dashboard/DashboardLayout.jsx
+++ src/dashboard/DashboardLayout.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import Graph from './Graph'
+import { dummyData } from '../dummyData/data'
 
 const DashboardLayout = () => {
   return (
     <div className='lg:px-14 sm:px-8 px-4 min-h-[calc(100vh-64px)]'>
         <div className='lg:w-[90%] w-full mx-auto py-16'>
             <div className='h-96 relative'>
-                
+                <Graph graphData={dummyData} />
             </div>
         </div>
     </div>
--- src/dashboard/Graph.jsx
+++ src/dashboard/Graph.jsx
@@ -32,7 +32,7 @@ const Graph = ({ graphData }) => {
       {
         label: "Total Clicks",
         data:
-         graphData.length > 0
+         graphData?.length > 0
             ? userPerDaya
             : [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 3, 2, 1],
         backgroundColor:
