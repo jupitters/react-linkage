2168fa110424785a4e30e25b8c9a2911b8b91782
jupitters <sampaiobale@gmail.com>
2026-02-28 14:33:14 -0300

    graph

--- src/dashboard/DashboardLayout.jsx
+++ src/dashboard/DashboardLayout.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const DashboardLayout = () => {
   return (
     <div className='lg:px-14 sm:px-8 px-4 min-h-[calc(100vh-64px)]'>
-        
+        <div className='lg:w-[90%] w-full mx-auto py-16'>
+            <div className='h-96 relative'>
+                
+            </div>
+        </div>
     </div>
   )
 }
