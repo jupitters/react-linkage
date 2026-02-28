d1d302e8928b626263cf768dd46d8674ff15ead9
jupitters <sampaiobale@gmail.com>
2026-02-28 14:02:40 -0300

    global context

---
+++ src/contextApi/ContextApi.jsx
@@ -0,0 +1,15 @@
+import { createContext, useState } from "react";
+
+const ContextApi = createContext()
+
+export const ContextProvider = ({ children }) => {
+    const getToken = localStorage.getItem("jwt_token") ? JSON.parse(localStorage.getItem("jwt_token")): null
+    const [token, setToken] = useState(getToken)
+
+    const sendData = {
+        token,
+        setToken,
+    }
+
+    return <ContextApi.Provider value={sendData}>{ children }</ContextApi.Provider>
+}
