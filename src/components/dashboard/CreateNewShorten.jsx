92ebafcd6e31e7423f35e3b778e2234228be674c
jupitters <sampaiobale@gmail.com>
2026-03-01 12:13:23 -0300

    shorten pop up modal

--- src/components/dashboard/CreateNewShorten.jsx
+++ src/components/dashboard/CreateNewShorten.jsx
@@ -1,6 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useStoredContext } from '../../contextApi/ContextApi';
+import { useForm } from 'react-hook-form';
+import { data } from 'autoprefixer';
+import TextField from '../TextField';
+import { Tooltip } from '@mui/material';
+import { RxCross2 } from 'react-icons/rx';
+import api from '../../api/api';
+import toast from 'react-hot-toast';
 
-const CreateNewShorten = () => {
+const CreateNewShorten = ({ setOpen, refetch }) => {
+    const { token } = useStoredContext();
+    const [loading, setLoading] = useState(false);
+
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors },
+    } = useForm({
+        defaultValues: {
+        originalUrl: "",
+        },
+        mode: "onTouched",
+    });
+
+    const createShortUrlHandler = async (data) => {
+    setLoading(true);
+    try {
+        const { data: res } = await api.post("/api/urls/shorten", data, {
+            headers: {
+              "Content-Type": "application/json",
+              Accept: "application/json",
+              Authorization: "Bearer " + token,
+            },
+          });
+
+          const shortenUrl = `${import.meta.env.VITE_REACT_FRONT_END_URL + "/s/" + `${res.shortUrl}`}`;
+          navigator.clipboard.writeText(shortenUrl).then(() => {
+            toast.success("Short URL Copied to Clipboard", {
+                position: "bottom-center",
+                className: "mb-5",
+                duration: 3000,
+            });
+          });
+
+          // await refetch();
+          reset();
+          setOpen(false);
+    } catch (error) {
+        toast.error(error);
+    } finally {
+        setLoading(false);
+    }
+  };
+
   return (
     <div>CreateNewShorten</div>
   )
