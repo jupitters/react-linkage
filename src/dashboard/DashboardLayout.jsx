cf5a439b6c4e7da52d20cfdcef5de26cb27016ab
jupitters <sampaiobale@gmail.com>
2026-03-01 10:48:26 -0300

    dashboard

--- src/dashboard/DashboardLayout.jsx
+++ src/dashboard/DashboardLayout.jsx
@@ -9,6 +9,11 @@ const DashboardLayout = () => {
             <div className='h-96 relative'>
                 <Graph graphData={dummyData} />
             </div>
+            <div className="py-5 sm:text-end text-center">
+              <button className='bg-custom-gradient px-4 py-2 rounded-md text-white'>
+                Create a New Short URL
+              </button>
+            </div>
         </div>
     </div>
   )
