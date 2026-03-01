e8f949cc4b41aa8a3d894ca986601548104e27c5
jupitters <sampaiobale@gmail.com>
2026-03-01 11:14:15 -0300

    query client

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,18 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { ContextProvider } from './contextApi/ContextApi.jsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const  queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <ContextProvider>
-        <App />
-      </ContextProvider>
+      <QueryClientProvider client={queryClient}>
+        <ContextProvider>
+          <App />
+        </ContextProvider>
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
 )
