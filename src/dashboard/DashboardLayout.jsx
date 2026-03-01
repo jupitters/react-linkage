994f7dcc55c1c86e2c19cdd00d88e4e7c5a5c001
jupitters <sampaiobale@gmail.com>
2026-03-01 11:12:12 -0300

    dashboard

--- src/dashboard/DashboardLayout.jsx
+++ src/dashboard/DashboardLayout.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import Graph from './Graph'
 import { dummyData } from '../dummyData/data'
+import { useStoredContext } from '../contextApi/ContextApi'
+import { useFetchTotalClicks } from '../hooks/useQuery'
 
 const DashboardLayout = () => {
+  const { token } = useStoredContext();
+
+  useFetchTotalClicks(token, onError);
+
+  const onError = (error) => {
+    console.log(error)
+  }
+
   return (
     <div className='lg:px-14 sm:px-8 px-4 min-h-[calc(100vh-64px)]'>
         <div className='lg:w-[90%] w-full mx-auto py-16'>
