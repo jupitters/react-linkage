1c7463da947942911c47c5160fb28ab2143b2104
jupitters <sampaiobale@gmail.com>
2026-03-02 14:07:02 -0300

    dashboard

--- src/components/dashboard/DashboardLayout.jsx
+++ src/components/dashboard/DashboardLayout.jsx
@@ -43,7 +43,18 @@ const DashboardLayout = () => {
             </div>
 
             <div>
-                
+                {!isLoading && myShortenUrls.length === 0 ? (
+                  <div className="flex justify-center pt-16">
+                    <div className="flex gap-2 items-center justify-center  py-6 sm:px-8 px-5 rounded-md   shadow-lg  bg-gray-50">
+                      <h1 className="text-slate-800 font-montserrat   sm:text-[18px] text-[14px] font-semibold mb-1 ">
+                        You haven't created any short link yet
+                      </h1>
+                      <FaLink className="text-blue-500 sm:text-xl text-sm " />
+                    </div>
+                  </div>
+                ) : (
+
+                )}
             </div>
         </div>
       )}
