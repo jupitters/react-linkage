d5b1a44b4c7f22f862531a3326a1ff600f2d1985
jupitters <sampaiobale@gmail.com>
2026-03-02 14:04:26 -0300

    fetch all my short urls

--- src/components/dashboard/DashboardLayout.jsx
+++ src/components/dashboard/DashboardLayout.jsx
@@ -2,17 +2,18 @@ import React, { useState } from 'react'
 import Graph from './Graph'
 import { dummyData } from '../../dummyData/data'
 import { useStoredContext } from '../../contextApi/ContextApi'
-import { useFetchTotalClicks } from '../../hooks/useQuery'
+import { useFetchMyShortUrls, useFetchTotalClicks } from '../../hooks/useQuery'
 import ShortenPopUp from './ShortenPopUp'
 
 const DashboardLayout = () => {
   const { token } = useStoredContext();
-  const refetch = false;
+  // const refetch = false;
   const [shortenPopUp, setShortenPopUp] = useState(false);
   const onError = (error) => {
     console.log(error)
   }
 
+  const { isLoading, data: myShortenUrls, refetch} = useFetchMyShortUrls(token, onError);
   const { isLoading: loader, data: totalClicks } = useFetchTotalClicks(token, onError);
 
   return (
@@ -39,6 +40,10 @@ const DashboardLayout = () => {
               <button className='bg-custom-gradient px-4 py-2 rounded-md text-white cursor-pointer' onClick={() => setShortenPopUp(true)}>
                 Create a New Short URL
               </button>
+            </div>
+
+            <div>
+                
             </div>
         </div>
       )}
