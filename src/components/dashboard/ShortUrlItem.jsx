03c552aaa9e3cc62e3603946713c1819980a29e4
jupitters <sampaiobale@gmail.com>
2026-03-02 14:22:43 -0300

    shorten url item

--- src/components/dashboard/ShortUrlItem.jsx
+++ src/components/dashboard/ShortUrlItem.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 
 const ShortUrlItem = ({ originalUrl, shortUrl, clickCount, createdDate }) => {
+    const subDomain = import.meta.env.VITE_REACT_FRONT_END_URL.replace(
+        /^https?:\/\//,
+        ""
+      );
+
   return (
-    <div>{shortUrl}</div>
+    <div className={`bg-slate-100 shadow-lg border border-dotted  border-slate-500 px-6 sm:py-1 py-3 rounded-md  transition-all duration-100 `}>
+        <div className={`flex sm:flex-row flex-col  sm:justify-between w-full sm:gap-0 gap-5 py-5 `}>
+            <div className="flex-1 sm:space-y-1 max-w-full overflow-x-auto overflow-y-hidden ">
+                <div className="text-slate-900 pb-1 sm:pb-0   flex items-center gap-2 ">
+                    <a href={`${import.meta.env.VITE_REACT_SUBDOMAIN}/${shortUrl}`}
+                        target="_blank"
+                        className=" text-[17px]  font-montserrat font-[600] text-linkColor ">
+                        {subDomain + "/" + `${shortUrl}`}
+                    </a>
+                </div>
+                
+            </div>
+        </div>
+    </div>
   )
 }
 
