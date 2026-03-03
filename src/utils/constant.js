93d0c6a53901b76dffa199729a3a887131351ffe
jupitters <sampaiobale@gmail.com>
2026-03-03 15:51:06 -0300

    subroutes

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,5 +1,6 @@
-import AppRouter from "../AppRouter";
+import AppRouter, { SubDomainRouter } from "../AppRouter";
 
 export const subDoaminList = [
-    {subdomain: "www", app: AppRouter}
+    {subdomain: "www", app: AppRouter, main:true},
+    {subdomain: "url", app: SubDomainRouter, main:false},
 ];
