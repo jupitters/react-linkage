155a3bf55d0ad30db806cfe3205705bf1aac4e7a
jupitters <sampaiobale@gmail.com>
2026-03-03 16:44:37 -0300

    private routes

--- src/PrivateRoute.jsx
+++ src/PrivateRoute.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import { Navigate } from "react-router-dom";
+import { useStoredContext } from "./contextApi/ContextApi";
 
-const PrivateRoute = () => {
-  return (
-    <div>PrivateRoute</div>
-  )
+export default PrivateRoute = ({ children, publicPage }) => {
+    const { token } = useStoredContext();
+
+    if(publicPage) {
+        return token ? <Navigate to="/dashboard" /> : children 
+    }
+    
+    return !token ? <Navigate to="/login" /> : children
 }
-
-export default PrivateRoute
