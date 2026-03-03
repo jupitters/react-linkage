5aa0aa66911a21f2688b87b62fbb7431c322e3db
jupitters <sampaiobale@gmail.com>
2026-03-03 15:35:07 -0300

    app router

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-
 const AppRouter = () => {
-  return (
-    <div>AppRouter</div>
-  )
+    return(
+        <>
+        <NavBar />
+            <Toaster />
+                <Routes>
+                    <Route path="/" element={<LandingPage />} />
+                    <Route path="/about" element={<AboutPage />} />
+                    <Route path="/register" element={<RegisterPage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/dashboard" element={<DashboardLayout />} />
+                </Routes>
+            <Footer />
+        </>
+    )
 }
 
 export default AppRouter
+
+export const SubDomainRouter = () => {
+    return (
+        <Routes>
+            <Route path="/:url" element={<LandingPage />} />   
+        </Routes>
+    )
+}
