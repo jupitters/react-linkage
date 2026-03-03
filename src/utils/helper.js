635041d1f12830224ac7be1fb8732474c0a74871
jupitters <sampaiobale@gmail.com>
2026-03-03 15:57:03 -0300

    get subdomain

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,3 +1,12 @@
 export const getApps = () => {
-    
+    const subdomain = getSubdomain(window.location.hostname)
+
+
+}
+
+export const getSubdomain = (location) => {
+    const locationParts = location.split(".")
+    const isLocalhost = location.slice(-1)[0] === "localhost"
+    const sliceTill = isLocalhost ? -1 : -2
+    return locationParts.slice(0, sliceTill).join("")
 }
