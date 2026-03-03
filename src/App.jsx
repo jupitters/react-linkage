1616cbdbce3441bf5ef2a35bc899e4a6f39f8357
jupitters <sampaiobale@gmail.com>
2026-03-03 16:07:19 -0300

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,5 @@
-import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import './App.css'
-import LandingPage from './components/LandingPage'
-import AboutPage from './components/AboutPage'
-import NavBar from './components/NavBar'
-import Footer from './components/Footer'
-import RegisterPage from './components/RegisterPage'
-import { Toaster } from 'react-hot-toast'
-import DashboardLayout from './components/dashboard/DashboardLayout'
-import LoginPage from './components/LoginPage'
+
 import { getApps } from './utils/helper'
 
 function App() {
--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,4 +1,13 @@
 import ShortenUrlPage from "./components/ShortenUrlPage"
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import LandingPage from './components/LandingPage'
+import AboutPage from './components/AboutPage'
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
+import RegisterPage from './components/RegisterPage'
+import { Toaster } from 'react-hot-toast'
+import DashboardLayout from './components/dashboard/DashboardLayout'
+import LoginPage from './components/LoginPage'
 
 const AppRouter = () => {
     return(
