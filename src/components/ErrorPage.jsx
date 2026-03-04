a68388a0c7c4a9d19f2a6d43fff003d995c0ecab
jupitters <sampaiobale@gmail.com>
2026-03-04 14:18:16 -0300

    error page

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { FaExclamationTriangle } from 'react-icons/fa'
 
 const ErrorPage = ({ message }) => {
   return (
-    <div>ErrorPage: {message} </div>
+    <div className='flex flex-col items-center justify-center min-h-[calc(100vh-64px)] bg-gray-100 p-6'>
+        <FaExclamationTriangle className='text-6xl text-red-500 mb-4' />
+        <h1 className='text-3xl font-bold mb-2 text-gray-800'>
+            Oops! Something went wrong.
+        </h1>
+    </div>
   )
 }
 
