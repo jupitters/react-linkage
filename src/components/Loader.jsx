0ad6a9c624f0966103592af2d489754568f6b4da
jupitters <sampaiobale@gmail.com>
2026-03-04 14:37:39 -0300

    loader component

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { RotatingLines } from 'react-loader-spinner'
 
 const Loader = () => {
   return (
-    <RotatingLines
+    <div className="flex justify-center items-center w-full h-[450px]">
+        <div className="flex flex-col items-center gap-1">
+        <RotatingLines
             visible={true}
             height="65"
             width="65"
@@ -12,7 +15,9 @@ const Loader = () => {
             ariaLabel="rotating-lines-loading"
             wrapperStyle={{}}
             wrapperClass=""
-            />
+        />
+        </div>
+    </div>
   )
 }
 
